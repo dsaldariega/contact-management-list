5aa3e9e2b1b0fdadc19e96556964c65eb72d5cc1
Dan Christopher Saldariega <dsaldariega@gmail.com>
2024-07-16 14:19:06 +0800

    update base url

--- src/api/contactApi.js
+++ src/api/contactApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000" || process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const getAllContacts = async () => {
   const response = await fetch(`${BASE_URL}/contacts`);
